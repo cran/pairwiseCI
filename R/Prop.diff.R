de8b490b0d2eca48aabcb8220e0a08e1dbea2dbe
Frank Schaarschmidt <schaarschmidt@biostat.uni-hannover.de>
2018-05-07 07:42:05 +0000

    version 0.1-26

--- R/Prop.diff.R
+++ R/Prop.diff.R
@@ -100,3 +100,140 @@ estimate=estimate)
 )
 }
 
+
+"NHS" <-
+  function(nx, ny, X, Y, quantile=qnorm(0.95), alternative="two.sided" )
+  {
+    
+    m<-nx
+    n<-ny
+    
+    # Wilson Score CI for one proportion:
+    
+    WilsonScore <- function(n,Y,quant=qnorm(0.95),alternative="two.sided") {
+      
+      t=Y/n
+      if(alternative =="two.sided") 
+      {   
+        est.int=(Y+(quant^2)/2)/(n+(quant)^2)
+        w.se=((quant)*sqrt(n*t*(1-t)+(quant^2)/4))/(n+quant^2)
+        KI=c( est.int-w.se, est.int+w.se )
+      }
+      else{
+        if(alternative=="less")
+        {   
+          est.int=(Y+(quant^2)/2)/(n+(quant)^2)
+          w.se=((quant)*sqrt(n*t*(1-t)+(quant^2)/4))/(n+quant^2)
+          KI=c( 0, est.int+w.se )
+        } 
+        else{
+          if(alternative=="greater")
+          {  
+            est.int=(Y+(quant^2)/2)/(n+(quant)^2)
+            w.se=((quant)*sqrt(n*t*(1-t)+(quant^2)/4))/(n+quant^2)
+            KI=c( est.int-w.se , 1 )
+          }
+          else{stop("argument alternative misspecified")}}}
+      
+      conf.int = KI
+      conf.int
+    }
+    
+    pX <- X/m
+    pY <- Y/n
+    estimate <- pX-pY
+    
+    
+    if(alternative=="two.sided")
+    {
+      quant<-quantile
+      CIX <- WilsonScore(n=m, Y=X, quant=quant, alternative="two.sided")
+      lX <- CIX[1]
+      uX <- CIX[2]
+      
+      CIY <- WilsonScore(n=n, Y=Y, quant=quant, alternative="two.sided") 
+      lY <- CIY[1]
+      uY <- CIY[2]
+      
+      conf.int <- c(estimate - quant * sqrt( (lX*(1-lX)/m) + (uY*(1-uY)/n) ) , 
+                    estimate + quant * sqrt( (uX*(1-uX)/m) + (lY*(1-lY)/n) ) )
+    }
+    
+    if(alternative=="less")
+    {
+      quant<-quantile
+      CIX <- WilsonScore(n=m, Y=X, quant=quant, alternative="less")
+      uX <- CIX[2]
+      
+      CIY <- WilsonScore(n=n, Y=Y, quant=quant, alternative="greater") 
+      lY <- CIY[1]
+      
+      
+      conf.int <- c( -1 , 
+                     estimate + quant * sqrt( (uX*(1-uX)/m) + (lY*(1-lY)/n) ) )
+    }
+    
+    
+    if(alternative=="greater")
+    {
+      quant<- (-quantile)
+      CIX <- WilsonScore(n=m, Y=X, quant=quant, alternative="greater")
+      lX <- CIX[1]
+      
+      
+      CIY <- WilsonScore(n=n, Y=Y, quant=quant, alternative="less") 
+      uY <- CIY[2]
+      
+      conf.int <- c(estimate - quant * sqrt( (lX*(1-lX)/m) + (uY*(1-uY)/n) ) , 1 )
+    }
+    
+    # if(all("two.sided", "less", "greater") != alternative)
+    #  { stop("argument alternative mis-specified") }
+    
+    
+    list(conf.int = conf.int,
+         estimate = estimate)
+    
+    # end of HybScore
+  }
+
+
+"Add4" <-
+  function(nx, ny, X, Y, quantile, alternative) 
+  {
+    
+    pxI <- (X+1)/(nx+2)
+    pyI <- (Y+1)/(ny+2)
+    
+    nxI <- nx+2
+    nyI <- ny+2
+    estI <- pxI - pyI
+    stderr <- sqrt( pxI*(1-pxI)/nxI + pyI*(1-pyI)/nyI )
+    
+    if(alternative=="two.sided")
+    {
+      lower <- estI - quantile*stderr
+      upper <- estI + quantile*stderr
+    }
+    
+    if(alternative=="less")
+    {
+      lower <- (-1)
+      upper <- estI + quantile*stderr
+    }
+    
+    if(alternative=="greater")
+    {
+      lower <- estI + quantile*stderr
+      upper <- 1
+    }
+    
+    px <- X/nx
+    py <- Y/ny
+    estimate <- px-py
+    
+    list(conf.int=c(lower,upper),
+         estimate=estimate)
+    
+  }
+
